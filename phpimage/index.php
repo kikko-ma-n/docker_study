6bf218b1893938be53e6de9745c89810e72f03f0
kikko-ma-n <soybean.shota@gmail.com>
2021-10-31 08:43:26 +0900

    Dockerfile-php

---
+++ phpimage/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>docker test</title>
+</head>
+<body>
+  Your IP <?php echo $_SERVER['REMOTE_ADDR'] ?>
+</body>
+</html>
